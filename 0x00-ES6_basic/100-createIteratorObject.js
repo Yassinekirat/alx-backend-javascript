743e97ff9f43ec8a0c1bae131f3f296f6c0bc3b6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-22 23:04:41 +0100

    task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function createIteratorObject(report) {
+  const iterator = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    iterator.push(...idx);
+  }
+  return iterator;
+}
