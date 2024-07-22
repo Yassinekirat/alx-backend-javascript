b396fd8c951e181ee1c90ed71f2f6df05f581dd0
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-22 22:54:21 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const nArray = [];
+  for (const idx of array) {
+    nArray.push(appendString + idx);
+  }
+  return nArray;
+}
