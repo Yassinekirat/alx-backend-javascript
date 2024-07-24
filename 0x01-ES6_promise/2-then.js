5b985ba41df5119924c17d5b5bf8561ed65f5125
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-24 20:10:30 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
+}
