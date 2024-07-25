bed64cd4ab2dfde0865ac54b2d7a88b387273ab6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-25 19:01:09 +0100

    fixing task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -13,7 +13,7 @@ export default class HolbertonCourse {
     if (typeof newName === 'string') {
       this._name = newName;
     } else {
-      TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
   }
 
@@ -25,7 +25,7 @@ export default class HolbertonCourse {
     if (typeof newLength === 'number') {
       this._length = newLength;
     } else {
-      TypeError('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
   }
 
@@ -37,7 +37,7 @@ export default class HolbertonCourse {
     if (newStudents instanceof Array) {
       this._students = newStudents;
     } else {
-      TypeError('Length must be a number');
+      throw new TypeError('Students must be an Array');
     }
   }
 }
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,43 @@
-#!/usr/bin/node
+export default class Currency {
+    constructor(name, code) {
+      this.name = name;
+      this.code = code;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      if (typeof newName === 'string') {
+        this._name = newName;
+      } else {
+        throw new TypeError('Name must be a string');
+      }
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set length(newLength) {
+      if (typeof newLength === 'number') {
+        this._length = newLength;
+      } else {
+        throw new TypeError('Length must be a number');
+      }
+    }
+  
+    get students() {
+      return this._students;
+    }
+  
+    set students(newStudents) {
+      if (newStudents instanceof Array) {
+        this._students = newStudents;
+      } else {
+        throw new TypeError('Length must be a number');
+      }
+    }
+  }
+  
