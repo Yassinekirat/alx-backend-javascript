7c135c811ce5ada8337f2a9ff9ca073cd02a7412
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-25 19:27:51 +0100

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,43 +1,30 @@
 export default class Currency {
-    constructor(name, code) {
-      this.name = name;
-      this.code = code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof newName === 'string') {
-        this._name = newName;
-      } else {
-        throw new TypeError('Name must be a string');
-      }
-    }
-  
-    get length() {
-      return this._length;
-    }
-  
-    set length(newLength) {
-      if (typeof newLength === 'number') {
-        this._length = newLength;
-      } else {
-        throw new TypeError('Length must be a number');
-      }
-    }
-  
-    get students() {
-      return this._students;
-    }
-  
-    set students(newStudents) {
-      if (newStudents instanceof Array) {
-        this._students = newStudents;
-      } else {
-        throw new TypeError('Length must be a number');
-      }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
     }
   }
-  
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    }
+  }
+
+  displayFullCurrency() {
+    return (`${this.code} (${this.name})`);
+  }
+}
