3f6b539d379909ea71f70374fe2a8990cf2109b4
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-31 01:41:40 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      // Find the grade record for the current student
+      const gradeRecord = newGrades.find((grade) => grade.studentId === student.id);
+
+      // Determine the grade to assign (either from the record or 'N/A')
+      const studentGrade = gradeRecord ? gradeRecord.grade : 'N/A';
+
+      // Return the student object with the assigned grade
+      return {
+        ...student,
+        grade: studentGrade,
+      };
+    });
+}
