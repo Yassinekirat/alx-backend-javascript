06a519b868bac304566bdb3fbcf68f5bdf1bd577
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-31 02:42:34 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+
+  const setRet = [...set]
+    .filter((s) => typeof s === 'string' && s.startsWith(startString))
+    .map((s) => s.slice(startString.length));
+
+  return setRet.join('-');
+}
