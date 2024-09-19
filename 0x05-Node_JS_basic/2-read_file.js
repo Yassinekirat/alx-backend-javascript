c2394ce12bcd00ca8a7a044005730bb98d5b7889
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-19 15:54:24 +0100

    lets see if the checkers pass

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, { encoding: 'utf8' }).split(/\r?\n/);
+    let totalStudents = 0;
+    const studentGroups = {};
+
+    data.forEach((line, index) => {
+      if (line.trim() !== '' && index > 0) {
+        totalStudents += 1;
+        const studentInfo = line.split(',');
+        const firstName = studentInfo[0];
+        const field = studentInfo[3];
+
+        if (!studentGroups[field]) {
+          studentGroups[field] = {
+            total: 1,
+            names: [firstName],
+          };
+        } else {
+          studentGroups[field].total += 1;
+          studentGroups[field].names.push(firstName);
+        }
+      }
+    });
+
+    console.log(`Number of students: ${totalStudents}`);
+    Object.entries(studentGroups).forEach(([field, group]) => {
+      console.log(`Number of students in ${field}: ${group.total}. List: ${group.names.join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
