75cb442d0a0db464a45ee6234c389172e9005b0f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-19 15:05:00 +0100

    fix task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  if (process.stdin.read()) {
-    process.stdout.write(`Your name is: ${process.stdin.read()}`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
