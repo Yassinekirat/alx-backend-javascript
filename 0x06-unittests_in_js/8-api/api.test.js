a8de40346412dc721cfe240c82276e39690a05ed
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 17:26:50 +0100

    getting some errors

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,19 @@
+const request = require('request');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const expect = chai.expect;
-const server = require('./api'); // Ensure the server is properly exported
-
-chai.use(chaiHttp);
 
 describe('Index page', () => {
   it('should return the correct status code', (done) => {
-    chai.request('http://localhost:7865')
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
   it('should return the correct message', (done) => {
-    chai.request('http://localhost:7865')
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
