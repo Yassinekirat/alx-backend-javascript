9f4accf749b3cf7b9dab5ce4334f942d461795c6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 17:30:03 +0100

    lets see

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+const express = require('express');
+const app = express();
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return the correct status code', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return the correct status code when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
