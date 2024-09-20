2a2548a7d1723fd6e0b90c011967b36b6c99dec6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 16:51:05 +0100

    task 6 Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+        return Promise.resolve({ data: 'Successful response from the API' });
+    }
+}
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function() {
+    it('should return a promise with the object {data: "Successful response from the API"} when success is true', function(done) {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                expect(response).to.have.property('data', 'Successful response from the API');
+                done();
+            })
+            .catch((error) => done(error));
+    });
+});
