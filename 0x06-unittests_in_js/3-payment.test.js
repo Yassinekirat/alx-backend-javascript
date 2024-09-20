ced3b4f3f5df473ae7aeee008f715782223fbd1e
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 16:28:21 +0100

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', function() {
+    it('should call Utils.calculateNumber with SUM and the correct arguments', function() {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+        spy.restore();
+    });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const Utils = {
+    calculateNumber: function(type, a, b) {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+        if (type === 'SUM') {
+            return roundedA + roundedB;
+        }
+        if (type === 'SUBTRACT') {
+            return roundedA - roundedB;
+        }
+        if (type === 'DIVIDE') {
+            if (roundedB === 0) return 'Error';
+            return roundedA / roundedB;
+        }
+        return null;
+    }
+};
+
+module.exports = Utils;
