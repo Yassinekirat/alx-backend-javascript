ce87a5f06bb91e1c044373e5f6ceea4aa20be3e3
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 17:44:50 +0100

    task 10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,58 @@
-#!/usr/bin/node
+const request = require('request');
+const { expect } = require('chai');
+
+
+describe('Index page', () => {
+  it('should return the correct status code', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return the correct status code when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('/available_payments', () => {
+  it('should return the correct payment methods', (done) => {
+    request.get('http://localhost:7865/available_payments', (error, response, body) => {
+      const expectedResponse = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false,
+        },
+      };
+      expect(response.statusCode).to.equal(200);
+      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
+      done();
+    });
+  });
+});
+
+describe('/login', () => {
+  it('should return the correct welcome message', (done) => {
+    request.post('http://localhost:7865/login', {
+      json: { userName: 'Betty' },
+    }, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
