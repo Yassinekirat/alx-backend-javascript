9a9d86022ea17d6f4fc272e9e194a488c7e2b6b9
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 16:44:13 +0100

    fix task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi with stub', function() {
+    let stub, spy;
+
+    beforeEach(() => {
+        stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+        spy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        stub.restore();
+        spy.restore();
+    });
+
+    it('should call Utils.calculateNumber with SUM and the correct arguments', function() {
+        sendPaymentRequestToApi(100, 20);
+
+        expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+        expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    });
+});
