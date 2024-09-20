209023bcdfc3633e2066ebe04baf97c3cbd7d230
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-20 17:01:04 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const express = require('express');
+const app = express();
+
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const server = require('./api'); // Ensure the server is properly exported
+
+chai.use(chaiHttp);
+
+describe('Index page', () => {
+  it('should return the correct status code', (done) => {
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should return the correct message', (done) => {
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
+});
